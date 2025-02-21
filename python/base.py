a4a8d19f3c59784a0e59fe639be3e58f1da694a3
eunsung <math.eunsung@gmail.com>
2025-02-21 14:32:39 +0900

    add some functions to base

--- python/base.py
+++ python/base.py
@@ -1,3 +1,13 @@
+from functools import reduce
+
+def is_square(n):
+    return int(n**0.5)**2 == n
+
+
+def is_palindrome(n):
+    return str(n) == str(n)[::-1]
+
+
 def all_primes_under(n):
     is_prime = [True] * (n+1)
     is_prime[0] = is_prime[1] = False
@@ -6,6 +16,15 @@ def all_primes_under(n):
             for j in range(i*i, n+1, i):
                 is_prime[j] = False
     return [i for i in range(n+1) if is_prime[i]]
+
+
+def get_prime_factors(n):
+    factors = []
+    for p in all_primes_under(n):
+        if n % p == 0:
+            factors.append(p)
+            n //= p
+    return factors
 
 if __name__ == "__main__":
     print(all_primes_under(100))
